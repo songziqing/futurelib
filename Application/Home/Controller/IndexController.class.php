c33ee5cb9de4df3dfeec53ca34ae2e7a28822b72
songziqing <1044649609@qq.com>
2016-02-15 14:52:11 +0800

    未来小组网站第一个版本

---
+++ Application/Home/Controller/IndexController.class.php
@@ -0,0 +1,25 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class IndexController extends Controller {
+    public function index(){
+        $this -> display();
+    }
+
+    public function about(){
+        $this -> display();
+    }
+
+    public function introduce(){
+        $this -> display();
+    }
+
+    public function news(){
+        $this -> display();
+    }
+
+    public function contact(){
+        $this -> display();
+    }
+
+}
